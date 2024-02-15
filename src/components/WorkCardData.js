5aba6e43be5c27f0b9c625300dd8b48e4bf17a38
Hrushi-00 <rushiskapse@gmail.com>
2024-02-15 22:19:58 +0530

    add project

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -1,10 +1,11 @@
 import pro1 from "../assets/ecommerce .png"
-import pro2 from "../assets/LibraryManagementProject.png"
-import pro3 from "../assets/calculator.png"
-import pro4 from "../assets/todo list.png"
-import pro5 from "../assets/proj4.png"
-import pro6 from "../assets/proj3.png"
-import pro7 from "../assets/proj2.png"
+import pro2 from "../assets/search image.png"
+import pro3 from "../assets/LibraryManagementProject.png"
+import pro4 from "../assets/calculator.png"
+import pro5 from "../assets/todo list.png"
+import pro6 from "../assets/proj4.png"
+import pro7 from "../assets/proj3.png"
+import pro8 from "../assets/proj2.png"
 
 const projectCardData=[
     {
@@ -16,37 +17,44 @@ const projectCardData=[
     },
     {
         imgsrc:pro2,
+        title:"Image Search website",
+        text:"Instead of typing a keyword or phrase into a search bar, the search engine retrieves similar or related images.Unlimited Search, explore freely. This process enhances user convenience, allowing them to explore the site without committing to immediate purchases. ",
+        
+        view:"https://hrushi-00.github.io/SearchImage/"
+    },
+    {
+        imgsrc:pro3,
         title:"LibraryManagementProject",
         text:"The Library Management System project aims to streamline the operations of a library by providing an efficient and user-friendly platform for librarians and patrons. The system encompasses essential features such as user authentication, allowing librarians and users to log in and register. It includes robust book management functionalities, enabling librarians to add, edit, and delete book records. Member management features facilitate the addition, modification, and removal of member records. ",
         
         view:"https://github.com/Hrushi-00/Library-Management-System"
     },
     {
-        imgsrc:pro3,
+        imgsrc:pro4,
         title:"Calculator",
         text:"The project is to develop a comprehensive and user-friendly task management system. The aim is to offer individuals a digital platform where they can efficiently organize, prioritize, and monitor their tasks. The system will empower users to create, edit, and delete tasks.",
         view:"https://hrushi-00.github.io/calculator/"
     },
     {
-        imgsrc:pro4,
+        imgsrc:pro5,
         title:"Todo List",
         text:"The ToDo list project aims to develop a user-friendly task management system, providing individuals with a digital platform to organize and prioritize their daily activities efficiently. The project incorporates essential features such as task creation, editing, and deletion, along with functionalities for setting task priorities, due dates, and organizing tasks into categories or tags. ",
         view:"https://hrushi-00.github.io/todo-list/"
     },
     {
-        imgsrc:pro5,
+        imgsrc:pro6,
         title:" ",
         text:"The website is built with a responsive layout, ensuring seamless access across various devices, including desktops, tablets",
         view:"https://hrushi-00.github.io/pro/"
     },
     {
-        imgsrc:pro6,
+        imgsrc:pro7,
         title:"Web Disign",
         text:"The website is built with a responsive layout, ensuring seamless access across various devices, including desktops, tablets ",
         view:"https://github.com/Hrushi-00/pro"
     },
     {
-        imgsrc:pro7,
+        imgsrc:pro8,
         title:"Web Disign",
         text:" Immerse yourself in the artistry of web design and development as showcased by our talented team of professionals.",
         view:"https://hrushi-00.github.io/hos/"
