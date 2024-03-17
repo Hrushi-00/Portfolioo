afa16ce0c2ae5e2c49239d46c93978d2401159ee
Hrushi-00 <rushiskapse@gmail.com>
2024-03-17 13:34:42 +0530

    update

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -4,12 +4,15 @@ import React from 'react'
 import React1 from "../assets/react2.avif"
 import React2 from "../assets/react6.jpg"
 const AboutContent = () => {
+  const linkdin = () =>{
+    window.open("https://www.linkedin.com/in/hrushikesh-kapse/")
+  }
   return (
     <div className="about">
      <div className="left"> 
      <h1>Who Am I?</h1>
-     <p>Im a react Front-end Developer.I create responsive secure websites from my clients</p>
-      <Link to="/contact"><button className="btn1">Contact</button></Link>
+     <p>Im a react Front-end Developer.I create responsive secure websites.</p>
+      <Link to="/contact"><button className="btn1" onClick={linkdin}>Contact</button></Link>
       </div>
        
      <div className="right">
